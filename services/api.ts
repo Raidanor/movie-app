f48f883501fd7aed510f2bc9a6d09814719b6e1c
Raidanor <silverstallion30@gmail.com>
2025-11-05 22:35:25 -0400

    tmdb api backend

---
+++ services/api.ts
@@ -0,0 +1,42 @@
+export const TMDB_CONFIG = {
+    BASE_URL: "https://api.themoviedb.org/3",
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+export const fetchMovies = async({ query }: {query: string}) => {
+    const endPoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                            : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+    const response = await fetch(endPoint, {
+        method: 'GET',
+        headers:    TMDB_CONFIG.headers,
+    })
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch movies", response.statusText)
+    }
+
+    const data = await response.json()
+
+    return data.results
+}
+
+
+// const url = 'https://api.themoviedb.org/3/movie/movie_id?language=en-US';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTJiNDIwZmE5NDFiZWZkODY5ODgwNGEzMGIyMDBmMSIsIm5iZiI6MTc2MjM5NDgyOS4xMzQsInN1YiI6IjY5MGMwMmNkYzY2OTMxYTQ4NWUxODk5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lH-uhTdqCIFRJ2u9fEfCPcYTZEBteLPYlMjP6KJbQ5A'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
+
