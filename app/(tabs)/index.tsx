326df83cc9ab974be55f024b41feebcfe573128d
Raidanor <silverstallion30@gmail.com>
2025-11-23 23:03:57 -0400

    few css fixes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -20,13 +20,13 @@ export default function Index() {
 
     return (
         <View className="flex-1 bg-primary">
-            <Image source={images.bg} className="absolute w-full z-0" />
+            <Image source={images.bg} className="absolute w-full" />
 
             <ScrollView className="flex-1 px-5 " 
                 showsVerticalScrollIndicator={false} 
                 contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}
             >
-                <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+                <Image source={icons.logo} className="w-12 h-10 mt-10 mb-5 mx-auto" />
 
                 { moviesLoading ? 
                 (
@@ -63,21 +63,21 @@ export default function Index() {
                                     marginBottom: 10,
 
                                 }}
-                                className="mt-2 pb-32"
+                                className="mt-2"
                                 scrollEnabled={false}
                             >
-
                             </FlatList>
                         </>
                     </View>
+                    
                 )}
                 
-                <View className="flex-1 mt-5">
+                {/* <View className="flex-1 mt-5">
                     <SearchBar 
                         onPress={() => router.push("/search")}
                         placeholder="Search for a movie"
                     />
-                </View>
+                </View> */}
 
             </ScrollView>
             
