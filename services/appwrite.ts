376936eaaf2602bd93d20679af2f7fa9f29c70c6
Raidanor <silverstallion30@gmail.com>
2025-12-14 13:32:29 -0400

    appwite user metrics

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,4 +1,4 @@
-import {Client, Databases, Query} from "react-native-appwrite"
+import {Client, Databases, ID, Query} from "react-native-appwrite"
 
 
 // track searches by user
@@ -13,16 +13,42 @@ const database = new Databases(client)
 
 
 export const updateSearchCount = async(query: string, movie: Movie) => {
-
-    const result = await database.listDocuments(DATABASE_ID, TABLE_ID, [
-        Query.equal('searchTerm', query)
-    ])
-
     // check if a record is found of that search has already been stored
     // if a document is found, increment the searchCount variable
     // if no document is found, create a new record in Appwrite database
 
-    console.log(result)
+    try {
+        const result = await database.listDocuments(DATABASE_ID, TABLE_ID, [
+            Query.equal('searchTerm', query)
+        ])
+
+
+
+        if (result.documents.length == 0){
+            const existingMovie = result.documents[0]
+
+            await database.updateDocument(
+                DATABASE_ID, 
+                TABLE_ID,
+                existingMovie.$id,
+                {
+                    count: existingMovie.count + 1
+                }
+            )
+        } else {
+            await database.createDocument(DATABASE_ID, TABLE_ID, ID.unique(), 
+            {
+                title: movie.title,
+                searchTerm: query,
+                movie_id: movie.id,
+                count: 1,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
 
 
 }
